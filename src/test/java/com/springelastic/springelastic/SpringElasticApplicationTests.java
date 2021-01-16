3f9af169cc93608ae44cb4f90e1be7e2210ef4eb
Abhishek Tanwar <abhishek.kumartanwar@hotmail.com>
2021-01-16 11:16:33 +0530

    initial

---
+++ src/test/java/com/springelastic/springelastic/SpringElasticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springelastic.springelastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringElasticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
